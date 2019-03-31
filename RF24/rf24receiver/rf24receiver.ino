0c0ad8157c56f9dc5957f733fc0bf9a6c545c0f9
Elias <panzercalva@gmail.com>
2019-03-31 18:02:30 +0200

    unsuccesful attempts

--- RF24/rf24receiver/rf24receiver.ino
+++ RF24/rf24receiver/rf24receiver.ino
@@ -1,16 +1,21 @@
 #include <RF24.h>
 #include <SPI.h>
+#include "nRF24L01.h"
 
-const int pinLed = A3;
 int ledState = LOW;          
 unsigned long previousMillis = 0;        
-const long interval = 1000;       
+const long interval = 400;       
 bool radioNumber = 0;
 bool role = 0;
 byte addresses[][6] = {"1Node","2Node"};
+int LED1 = 5;
+int msg[1];
+unsigned long counter, rxTimer;
+unsigned long startTime, stopTime; 
 
 RF24 radio(7,8);
 
+byte data[32];     
 
 void setup()
 {
@@ -19,27 +24,30 @@ void setup()
     Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
   
     radio.begin();
-    radio.setPALevel(RF24_PA_HIGH);
+    radio.setPALevel(RF24_PA_MAX);
     radio.openWritingPipe(addresses[0]);
     radio.openReadingPipe(1,addresses[1]);
+    radio.setChannel(1);
     radio.startListening();
+    pinMode(LED1, OUTPUT);
 }
-
 
 void loop()
 {
-    unsigned long got_time;
-    
-    if( radio.available()){
-                                                                    // Variable for the received timestamp
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
-      }
-     
-      radio.stopListening();                                        // First, stop listening so we can talk   
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
-      Serial.print(F("Sent response "));
-      Serial.println(got_time);  
+     while(radio.available())
+     {       
+      radio.read(&data,32);
+      counter++;
+     }
+   if(millis() - rxTimer > 1000)
+   {
+     rxTimer = millis();     
+     unsigned long numBytes = counter*32;
+     Serial.print(F("Rate: "));
+     //Prevent dividing into 0, which will cause issues over a period of time
+     Serial.println(numBytes > 0 ? numBytes/1000.0:0);
+     Serial.print(F("Payload Count: "));
+     Serial.println(counter);
+     counter = 0;
    }
 }
--- RF24/rf24sender/rf24sender.ino
+++ RF24/rf24sender/rf24sender.ino
@@ -1,31 +1,96 @@
 #include <RF24.h>
 #include <SPI.h>
+#include "nRF24L01.h"
 
+unsigned long counter;
+unsigned long startTime, stopTime; 
 const int pinLed = A3;
 int ledState = LOW;          
 unsigned long previousMillis = 0;        
 const long interval = 1000;       
 bool radioNumber = 0;
 bool role = 0;
-byte addresses[][6] = {"1Node","2Node"};
 
+byte data[32];     
+
+const uint64_t pipes[2] = { 0xABCDABCD71LL, 0x544d52687CLL };   // Radio pipe addresses for the 2 nodes to communicate.
+
+int msg[1];
 RF24 radio(7,8);
-
+int SW1 = 7;
 
 void setup()
 {
     pinMode(pinLed, OUTPUT);
-    Serial.begin(115200);
-    Serial.println(F("RF24/examples/GettingStarted"));
-    Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
+  Serial.begin(115200);
+  radio.begin();                           // Setup and configure rf radio
+  radio.setChannel(1);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setAutoAck(1);                     // Ensure autoACK is enabled
+  radio.setRetries(2,15);                  // Optionally, increase the delay between retries & # of retries
   
-    radio.begin();
-    radio.setPALevel(RF24_PA_HIGH);
-    radio.openWritingPipe(addresses[0]);
-    radio.openReadingPipe(1,addresses[1]);
-    radio.startListening();
+  radio.setCRCLength(RF24_CRC_8);          // Use 8-bit CRC for performance
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+  
+  radio.startListening();                 // Start listening
+  radio.printDetails();                   // Dump the configuration of the rf unit for debugging
+  
+  Serial.println(F("\n\rRF24/examples/Transfer/"));
+  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
+  
+  randomSeed(analogRead(0));              //Seed for random number generation
+  
+  for(int i=0; i<32; i++){
+     data[i] = random(255);               //Load the buffer with random data
+  }
+  radio.powerUp(); 
 }
 
+
+void loop()
+{
+        delay(2000);
+    
+    Serial.println(F("Initiating Basic Data Transfer"));
+    
+    
+    unsigned long cycles = 10000; //Change this to a higher or lower number. 
+    
+    startTime = millis();
+    unsigned long pauseTime = millis();
+            
+    for(int i=0; i<cycles; i++){        //Loop through a number of cycles
+      data[0] = i;                      //Change the first byte of the payload for identification
+      if(!radio.write(&data,32)){   //Write to the FIFO buffers        
+        counter++;                      //Keep count of failed payloads
+      }
+      
+      //This is only required when NO ACK ( enableAutoAck(0) ) payloads are used
+//      if(millis() - pauseTime > 3){
+//        pauseTime = millis();
+//        radio.txStandBy();          // Need to drop out of TX mode every 4ms if sending a steady stream of multicast data
+//        //delayMicroseconds(130);     // This gives the PLL time to sync back up   
+//      }
+      
+    }
+    
+   stopTime = millis();   
+                                         //This should be called to wait for completion and put the radio in standby mode after transmission, returns 0 if data still in FIFO (timed out), 1 if success
+
+   //radio.txStandBy(1000);              //Standby, using extended timeout period of 1 second
+   
+   float numBytes = cycles*32;
+   float rate = numBytes / (stopTime - startTime);
+    
+   Serial.print("Transfer complete at "); Serial.print(rate); Serial.println(" KB/s");
+   Serial.print(counter); Serial.print(" of "); Serial.print(cycles); Serial.println(" Packets Failed to Send");
+   counter = 0;   
+}
+
+
+/*
 void loop()
 {
     unsigned long currentMillis = millis();
@@ -77,3 +142,4 @@ void loop()
     // Try again 1s later
     delay(1000);
 }
+*/

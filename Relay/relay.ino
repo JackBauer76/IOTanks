40decb5be53fc43cc232835e4a64a0233da53e6a
Elias <panzercalva@gmail.com>
2019-09-14 19:06:57 +0200

    New Relay sketch

---
+++ Relay/relay.ino
@@ -0,0 +1,37 @@
+    // ATMEL ATTINY 25/45/85 / ARDUINO
+    //
+    //                  +-\/-+
+    //      Ain0 (D 5) PB5  1|    |8  Vcc
+    // PIR  Ain3 (D 3) PB3  2|    |7  PB2 (D 2) Ain1     Neopixel
+    //      Ain2 (D 4) PB4  3|    |6  PB1 (D 1) pwm1     Rotor
+    //                 GND  4|    |5  PB0 (D 0) pwm0     Pushbutton
+    //      
+
+
+const int Pushbutton = 2;
+const int Relay = 7;
+const int Pir = 3;
+
+void setup()
+{
+    pinMode(Pushbutton, INPUT_PULLUP);
+    pinMode(Relay, OUTPUT);
+    pinMode(Pir, INPUT);
+}
+
+void loop()
+{
+    
+    if (digitalRead(Pir))
+    {
+        digitalWrite(Relay, HIGH);
+    }
+    else
+    {
+        digitalWrite(Relay, LOW);
+    }
+    
+    //delay(1500);
+    
+    //delay(1200);
+}
